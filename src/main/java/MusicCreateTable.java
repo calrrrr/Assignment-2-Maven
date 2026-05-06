f36e1c40947a475a326249c98dfabb6bb09cc670
calrrrr <s4076955@student.rmit.edu.au>
2026-05-06 18:06:27 +1000

    dynamoDB deployment on AWS

--- src/main/java/MusicCreateTable.java
+++ src/main/java/MusicCreateTable.java
@@ -14,11 +14,15 @@ public class MusicCreateTable {
 
     public static void main(String[] args) throws Exception {
 
+//        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+//                .withEndpointConfiguration(
+//                        new AwsClientBuilder.EndpointConfiguration(
+//                                "http://localhost:8000",
+//                                Regions.US_EAST_1.getName()))
+//                .build();
+
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-                .withEndpointConfiguration(
-                        new AwsClientBuilder.EndpointConfiguration(
-                                "http://localhost:8000",
-                                Regions.US_EAST_1.getName()))
+                .withRegion(Regions.US_EAST_1)
                 .build();
 
         DynamoDB dynamoDB = new DynamoDB(client);
